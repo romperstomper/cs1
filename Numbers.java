e0fd177ea549037979ff233efea08263741ff2e3
Gary <garyboland@gmail.com>
2014-10-04 13:12:59 +0100

    updated string formatting to 2 decimal places

--- Numbers.java
+++ Numbers.java
@@ -21,6 +21,6 @@ public class Numbers {
 	int pence = oldmoneyScanner.nextInt();
 	double newmoney = (double) (pence*0.67 + shillings*12*0.67 + pounds*240*0.67);
 	 
-	JOptionPane.showMessageDialog(null, "Your money in Euro is " + newmoney);
+	JOptionPane.showMessageDialog(null, String.format("Your money in Euro is %.2f", newmoney));
           }
     }
